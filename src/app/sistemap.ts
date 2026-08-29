920797993e861ddcc564b339f128cec07daf3919
jeanlucassolutions <jeanlucas.solutions@gmail.com>
2026-08-29 15:38:05 -0300

    Update sitemap URL to new domain

--- src/app/sistemap.ts
+++ src/app/sistemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = 'https://jean-lucas-eletricista.vercel.app'
+  const siteUrl = 'https://jeanlucaseletricista.com.br'
 
   return [
     {
