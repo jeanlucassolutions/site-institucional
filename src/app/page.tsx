25c27746cdf6a20e476639d772b9354a6ec571a9
jeanlucassolutions <jeanlucas.solutions@gmail.com>
2026-08-28 23:26:19 -0300

    Montar página principal do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,37 @@
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Services from '../components/Services'
+import Problems from '../components/Problems'
+import Differentials from '../components/Differentials'
+import About from '../components/About'
+import ServiceArea from '../components/ServiceArea'
+import Testimonials from '../components/Testimonials'
+import FAQ from '../components/FAQ'
+import FinalCTA from '../components/FinalCTA'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import FloatingWhatsApp from '../components/FloatingWhatsApp'
 
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+        <Services />
+        <Problems />
+        <Differentials />
+        <About />
+        <ServiceArea />
+        <Testimonials />
+        <FAQ />
+        <FinalCTA />
+        <Contact />
+      </main>
+
+      <Footer />
+      <FloatingWhatsApp />
+    </>
+  )
+}
