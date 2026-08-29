9aacf7816ca12caff31ceb3ba771e267c5849dfe
jeanlucassolutions <jeanlucas.solutions@gmail.com>
2026-08-28 23:43:26 -0300

    Desativar ESLint durante o build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+
   headers: async () => {
     return [
       {
@@ -35,6 +41,7 @@ const nextConfig = {
       },
     ]
   },
+
   compress: true,
   swcMinify: true,
   poweredByHeader: false,
